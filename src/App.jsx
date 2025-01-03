fa2f96908786a1441d9bdf6f653e531be718b581
Sofia <katzmansof@gmail.com>
2025-01-02 20:59:25 -0500

    update to temp riso data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import Risograph from './component/Risograph'
+import RISO1 from './risographs/RISO1'
+import RISO2 from './risographs/RISO2'
+import RISO3 from './risographs/RISO3'
+
 
 function App() {
 
   return (
     <div className="main">
-      <Risograph />
-      <Risograph />
-      <Risograph />
+      <Risograph data={RISO1} />
+      <Risograph data={RISO2} />
+      <Risograph data={RISO3} />
 
     </div>
   )
